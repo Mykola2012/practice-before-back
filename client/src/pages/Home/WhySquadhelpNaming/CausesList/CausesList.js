4091697156b887aae938bf403aba593fca3accd5
Revika Mukola <revikanikolaj@gmail.com>
2022-05-16 11:08:29 +0200

    Create CausesList

--- client/src/pages/Home/WhySquadhelpNaming/CausesList/CausesList.js
+++ client/src/pages/Home/WhySquadhelpNaming/CausesList/CausesList.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import articles from './articles.json';
 import styles from './CausesList.module.sass';
 
 function CausesList () {
-  return <div>CausesList</div>;
+  const mapBodyPart2 = a => <p>{a.bodyPart2}</p>;
+
+  const mapArticles = (a, i) => (
+    <li key={i}>
+      <h3>{a.title}</h3>
+      <i className={a.iconClass}></i>
+      <p>{a.bodyPart1}</p>
+      <Link to='#'>{a.linkText}</Link>
+      {articles.map(mapBodyPart2)}
+    </li>
+  );
+
+  return <ul>{articles.map(mapArticles)}</ul>;
 }
 
 export default CausesList;
