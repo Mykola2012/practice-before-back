425223a8286aaa25fc472490822c52698219bce6
Revika Mukola <revikanikolaj@gmail.com>
2022-05-16 11:28:27 +0200

    Refactoring  CausesList

--- client/src/pages/Home/WhySquadhelpNaming/CausesList/CausesList.js
+++ client/src/pages/Home/WhySquadhelpNaming/CausesList/CausesList.js
@@ -4,19 +4,32 @@ import articles from './articles.json';
 import styles from './CausesList.module.sass';
 
 function CausesList () {
-  const mapBodyPart2 = a => <p>{a.bodyPart2}</p>;
+  const mapBodyPart2 = a => <p className={styles.textBody}>{a.bodyPart2}</p>;
 
-  const mapArticles = (a, i) => (
-    <li key={i}>
-      <h3>{a.title}</h3>
-      <i className={a.iconClass}></i>
-      <p>{a.bodyPart1}</p>
-      <Link to='#'>{a.linkText}</Link>
-      {articles.map(mapBodyPart2)}
-    </li>
+  const mapArticles = (a, i) => {
+    const { title, bodyPart1, linkText, iconClass, color, colorBg } = a;
+
+    const stylesIcon = {
+      color: color,
+      backgroundColor: colorBg,
+    };
+
+    return (
+      <li key={i} className={styles.listItemContainer}>
+        <h3 className={styles.subTitle}>{title}</h3>
+        <i className={iconClass} style={stylesIcon}></i>
+        <div className={styles.bodyWrapper}>
+          <p className={styles.textBody}>{bodyPart1}</p>
+          <Link to='#'>{linkText}</Link>
+          {articles.map(mapBodyPart2)}
+        </div>
+      </li>
+    );
+  };
+
+  return (
+    <ul className={styles.articleContainer}>{articles.map(mapArticles)}</ul>
   );
-
-  return <ul>{articles.map(mapArticles)}</ul>;
 }
 
 export default CausesList;
