a3955ecd066d07c456810727eca5b1edc098b7df
Revika Mukola <revikanikolaj@gmail.com>
2022-05-17 11:55:21 +0200

    Add styles for section WhySquadhelp

--- client/src/pages/Home/WhySquadhelpNaming/WhySquadhelpNaming.js
+++ client/src/pages/Home/WhySquadhelpNaming/WhySquadhelpNaming.js
@@ -2,25 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import CausesList from './CausesList/CausesList';
 import styles from './WhySquadhelpNaming.module.sass';
+import pointer from '../../../img/pointer.png';
 
 function WhySquadhelpNaming () {
   return (
-    <>
-      <h2>Why Squadhelp Naming Services?</h2>
+    <section className={styles.sectionWrapper}>
+      <h2 className={styles.title}>Why Squadhelp Naming Services?</h2>
       <CausesList />
-      <div>
-        <img src='' alt='' />
-        <img src='' alt='' />
-        <img src='' alt='' />
+      <div className={styles.imgWrapper}>
+        <img src={pointer} alt='Pointer' className={styles.imgPointer} />
       </div>
-      <h3>World's #1 Naming Website</h3>
-      <p>
+
+      <h3 className={styles.subTitle}>World's #1 Naming Website</h3>
+      <p className={styles.textBody}>
         Recognized as one of the <Link to='#'>Most Innovative Companies</Link>{' '}
         by Inc Magazine
       </p>
-      <a href='#'>See How It Works</a>
-      <span>Over 25,000+ Customers</span>
-    </>
+      <a className={styles.linkBtn} href='#'>
+        See How It Works
+      </a>
+      <span className={styles.textSpan}>Over 25,000+ Customers</span>
+    </section>
   );
 }
 
